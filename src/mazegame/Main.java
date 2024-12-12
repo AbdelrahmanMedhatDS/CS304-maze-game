1a10184be4129b0d99396f5054ab34b65f7619cc
Abdelrahman Medhat <a.medhatahmed9@gmail.com>
2024-12-13 01:57:07 +0200

    Initial commit

---
+++ src/mazegame/Main.java
@@ -0,0 +1,11 @@
+package mazegame;
+
+import mazegame.scenes.MainMenuScene;
+
+public class Main {
+    public static void main(String[] args) {
+        MainMenuScene menu = new MainMenuScene();
+        menu.start();
+
+    }
+}
