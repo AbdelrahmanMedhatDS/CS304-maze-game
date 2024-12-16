179a08c6d22e39a130a84ac6a61503245f57ea86
MohamedWyel <137799932+MohamedWyel@users.noreply.github.com>
2024-12-16 07:57:04 +0200

    Add files via upload

---
+++ CS304-maze-game-main/src/mazegame/Main.java
@@ -0,0 +1,18 @@
+package mazegame;
+
+import mazegame.scenes.MainMenuScene;
+
+public class Main {
+    public static void main(String[] args) {
+        MainMenuScene menu = new MainMenuScene();
+        menu.start();
+        //level selection
+        //single player
+        //GUI
+        //username(s)
+        //high scores
+        //win-lose
+        //monsters
+        //music (DONE) by wael
+    }
+}
